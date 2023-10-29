471ec6801e7a11ba8adc6997205d11e8ab676c6a
sYimyk3 <syim7k.dev@gmail.com>
2023-10-29 15:11:48 +0600

    header

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './main.scss'
+
+export default function Main() {
+  return (
+    <div>
+        <div className="container">
+            <div className="top-Line">
+                dsdf
+            </div>
+        </div>
+    </div>
+  )
+}
